1da2190e334b462d5c1281d7b9c4f471d8fa46d1
John Day <itisjohnday@gmail.com>
2017-09-10 21:36:31 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,7 +8,7 @@ WIN_COMBINATIONS = [
   [0,1,2], # Top row
   [3,4,5], # Middle row
   [6,7,8], # Bottom row
-  [1,3,6], # Left Column
+  [0,3,6], # Left Column
   [1,4,7], # Middle Column
   [2,5,8], # Right Column
   [0,4,8], # 1st Diagonal
